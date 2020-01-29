9b0aa4babc80007b877c416e499b0c7d53c55a94
mustapha <m.touil.mt@gmail.com>
2020-01-29 09:43:57 +0100

    mise en commentaire de la classe users et sites suite problème de relation de table

--- LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/dao/SiteRepository.java
+++ LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/dao/SiteRepository.java
@@ -1,19 +1,17 @@
 package com.LesAmisDeLEscalade.dao;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.LesAmisDeLEscalade.entities.Site;
 
 @Repository
-public interface SiteRepository extends PagingAndSortingRepository <Site,Long> {
+public interface SiteRepository extends JpaRepository <Site,Long> {
 
 	  @Query("select s from Site s where s.nom like :x") 
 	  
