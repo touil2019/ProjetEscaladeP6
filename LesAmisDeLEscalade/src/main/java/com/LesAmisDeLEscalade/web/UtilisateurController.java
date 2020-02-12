100304f6f3488e0a2b90efb9a6dcc7d77564087c
mustapha <m.touil.mt@gmail.com>
2020-02-12 10:07:53 +0100

    concordances des champs utilisateur controller et vues.
    supp page admin
    
    creation utilisateur admin et user
    
    renommer le formulaire pour les sites

--- LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/web/UtilisateurController.java
+++ LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/web/UtilisateurController.java
@@ -1,8 +1,16 @@
 package com.LesAmisDeLEscalade.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.LesAmisDeLEscalade.dao.UtilisateurRepository;
+import com.LesAmisDeLEscalade.entities.Utilisateur;
 
 
 @Controller
@@ -10,5 +18,21 @@ public class UtilisateurController {
 
 	@Autowired
 	private UtilisateurRepository utilisateurRepository;
+	
+	@RequestMapping(value = "/utilisateur/creer", method = RequestMethod.GET)
+	public String creerUtilisateur(Model model) {
+		Utilisateur utilisateur = new Utilisateur();
+		model.addAttribute("utilisateur", utilisateur);
+		return "CreerUtilisateur";
+	}
 
+	@RequestMapping(value = "/utilisateur/save", method = RequestMethod.POST)
+	public String saveUtilisateur(Model model, @Valid Utilisateur utilisateur, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "CreerUtilisateur";
+		}
+		utilisateurRepository.save(utilisateur);
+		
+		return "Profil";
+	}
 }
