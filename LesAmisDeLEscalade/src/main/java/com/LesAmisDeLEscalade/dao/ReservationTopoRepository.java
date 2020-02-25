7b7bd080ae001dd43086f62c9b73c27dca8ddca5
mustapha <m.touil.mt@gmail.com>
2020-02-25 11:12:09 +0100

    mise en place de la fonctionnalite reservation
    avec modi du repo reservation avec mise en place d une query avec un
    lien avec les topo utilisateurs et l id

--- LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/dao/ReservationTopoRepository.java
+++ LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/dao/ReservationTopoRepository.java
@@ -1,10 +1,18 @@
 package com.LesAmisDeLEscalade.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.LesAmisDeLEscalade.entities.ReservationTopo;
+
 @Repository
 public interface ReservationTopoRepository extends JpaRepository<ReservationTopo,Long>{
+	
+	@Query("select r from  r where r.topo.utilisateur.id=:id")
+	public List<ReservationTopo> listeDeReservationParUsers(@Param("id") Long id);
 
 }
--- LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/web/UtilisateurController.java
+++ LesAmisDeLEscalade/src/main/java/com/LesAmisDeLEscalade/web/UtilisateurController.java
@@ -7,14 +7,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.LesAmisDeLEscalade.dao.SiteRepository;
 import com.LesAmisDeLEscalade.dao.UtilisateurRepository;
-import com.LesAmisDeLEscalade.entities.Topo;
 import com.LesAmisDeLEscalade.entities.Utilisateur;
 
 
